e3db674b383ed5985429a9d6c59ab3e869e01d2f
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2025-12-22 18:44:27 +0100

    fix coderabbit suggestion

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <header>
             <div className="main-container inner">
                 <Link href="/">
-                    <Image src="logo.svg" alt="CoinPulse logo" width={132} height={40} />
+                    <Image src="/logo.svg" alt="CoinPulse logo" width={132} height={40} />
                 </Link>
 
                 <nav>
